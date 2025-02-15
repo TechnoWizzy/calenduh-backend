24552a3b86f1867b9428bbe572da51cdf55e8172
eborszem <eborszem@purdue.edu>
2025-02-14 20:01:41 -0500

    Work for groups routing and handling

---
+++ internal/handlers/group.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"calenduh-backend/internal/database"
+	"net/http"
+)
+
+func CreateGroup(c *gin.Context) {
+	var options database.CreateGroupOptions
+	if err := c.ShouldBindJSON(&options); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	group, err := database.CreateGroup(c, &options)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, group)
+}
+
+func FetchGroup(c *gin.Context) {
+	id := c.Query("id")
+	group, err := database.FetchGroupById(c, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to fetch group"})
+		return
+	}
+	c.JSON(http.StatusOK, group)
+}
+
+func FetchAllGroups(c *gin.Context) {
+	groups, err := database.FetchAllGroups(c)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to fetch all groups"})
+		return
+	}
+	c.JSON(http.StatusOK, groups)
+}
+
+func UpdateGroup(c *gin.Context) {
+	id := c.Query("id")
+	var options database.UpdateGroupOptions
+	if err := c.ShouldBindJSON(&options); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	group, err := database.UpdateGroup(c, id, &options)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to update group"})
+		return
+	}
+	c.JSON(http.StatusOK, group)
+}
