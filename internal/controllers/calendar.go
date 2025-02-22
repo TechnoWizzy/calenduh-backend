9be849830f622e332d345a2e3dd55a046aa5e33f
eborszem <eborszem@purdue.edu>
2025-02-22 17:34:22 -0500

    Added queries + functions for calendar updating, creating, and deleting (for one calendar + for all calendars under a specific user id)

--- internal/controllers/calendar.go
+++ internal/controllers/calendar.go
@@ -52,3 +52,75 @@ func FetchGroupCalendars(c *gin.Context, user *sqlc.User) {
 func FetchSubscribedCalendars(c *gin.Context) {
 
 }
+
+func CreateCalendar(c *gin.Context) {
+	var input sqlc.CreateCalendarParams
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid input" + err.Error()})
+		return
+	}
+
+	err := database.Queries.CreateCalendar(c, input)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to create calendar" + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "calendar created successfully"})
+}
+
+func DeleteCalendar(c *gin.Context) {
+	calendarId := c.Param("calendar_id")
+	if calendarId == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "calendar_id is required"})
+		return
+	}
+
+	err := database.Queries.DeleteCalendar(c, calendarId)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to delete calendar" + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "calendar deleted successfully"})
+}
+
+func DeleteAllUserCalendars(c *gin.Context) {
+	userId := c.Param("user_id")
+	if userId == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "user_id is required"})
+		return
+	}
+
+	err := database.Queries.DeleteAllUserCalendars(c, &userId)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to delete calendars" + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "calendars deleted successfully"})
+}
+
+func UpdateCalendar(c *gin.Context) {
+	calendarId := c.Param("calendar_id")
+	if calendarId == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "calendar_id is required"})
+		return
+	}
+
+	var input sqlc.UpdateCalendarParams
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid input" + err.Error()})
+		return
+	}
+
+	input.CalendarID = calendarId
+
+	err := database.Queries.UpdateCalendar(c, input)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to update calendar" + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "calendar updated successfully"})
+}
--- postgres/queries/calendars.sql
+++ postgres/queries/calendars.sql
@@ -15,3 +15,24 @@ select distinct c.* from users u
 inner join subscriptions s on u.user_id = s.user_id
 inner join calendars c on s.calendar_id = c.calendar_id
 where u.user_id = $1;
+
+-- name: CreateCalendar :exec
+INSERT INTO calendars (
+    calendar_id, user_id, group_id, title, is_public
+)
+VALUES (
+    $1, $2, $3, $4, $5
+);
+
+-- name: DeleteCalendar :exec
+DELETE FROM calendars
+WHERE calendar_id = $1;
+
+-- name: DeleteAllUserCalendars :exec
+DELETE FROM calendars
+WHERE user_id = $1;
+
+-- name: UpdateCalendar :exec
+UPDATE calendars
+SET title = $1, is_public = $2, user_id = $3, group_id = $4
+WHERE calendar_id = $5;
