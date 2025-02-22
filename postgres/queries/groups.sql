f1d5423a35bc9a8cd633327b73691ea87839d7ab
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-02-22 13:32:59 -0500

    Fix sql bug

--- postgres/queries/groups.sql
+++ postgres/queries/groups.sql
@@ -1,5 +1,5 @@
 -- name: FetchGroupsByUserId :many
 select g.* from users u
 inner join group_members gm on u.user_id = gm.user_id
-inner join groups g on gm.group_id = g.id
+inner join groups g on gm.group_id = g.group_id
 where u.user_id = $1;
