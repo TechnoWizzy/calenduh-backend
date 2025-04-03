6296167a476ddfbfb8f7d10db69c4b9904a325b0
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-02 23:15:01 -0400

    Fix COrs

--- internal/util/util.go
+++ internal/util/util.go
@@ -65,7 +65,8 @@ func TimeToMidnightEST() time.Duration {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
