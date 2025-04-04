eba7b5548ac9f18f959e365343cb244652f69244
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-04 01:00:08 -0400

    FIx CORS

--- main.go
+++ main.go
@@ -188,7 +188,13 @@ func cleanup(server *http.Server) {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		origin := c.Request.Header.Get("Origin")
+		log.Println("origin: ", origin)
+		if origin == "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
