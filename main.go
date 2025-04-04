bd7d5dfc744e0263a429655b9896a592971b67de
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-03 23:51:33 -0400

    Test

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 
 	// Router setup
 	router := gin.Default()
-	router.Use(cors.New(config))
+	router.Use(CORSMiddleware())
 	router.Use(gin.Recovery())
 	router.Use(controllers.Authorize)
 	router.GET("/health", func(c *gin.Context) {
@@ -183,5 +183,21 @@ func cleanup(server *http.Server) {
 		log.Println("server shutdown failed:", err)
 	} else {
 		log.Println("server shutdown successfully")
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
