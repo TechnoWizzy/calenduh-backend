5f029634e95aec3e71e5ca32ccc8919ba759eb7c
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-29 14:54:50 -0400

    Uh oh \r\n didn't work!

--- internal/controllers/calendar.go
+++ internal/controllers/calendar.go
@@ -132,7 +132,7 @@ func GetCalendarICal(c *gin.Context, calendarId string) {
 		}
 	}
 
-	data := cal.Serialize("\r\n")
+	data := cal.Serialize("\n")
 	c.Header("Content-Type", "text/calendar; charset=utf-8")
 	c.Header("Content-Disposition", "attachment; filename=\"calendar.ics\"")
 	c.Header("Cache-Control", "no-cache, no-store, must-revalidate") // Prevent aggressive caching
